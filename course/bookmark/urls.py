901541c38cb3949dab49504e0b3134d8fbfcac96
Sya-Sya <58839174+Sya-Sya@users.noreply.github.com>
2020-01-25 13:36:51 +0545

    File not working

--- course/bookmark/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from bookmark import views
-
-urlpatterns = [
-    path('bookmark', views.bookmark, name='bookmark'),
-    path('home',views.home, name='home'),
-    path('login',views.login, name='login')
-]
