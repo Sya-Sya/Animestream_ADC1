f330711abff89c4f9dcf1f0c742ef654e70c2b62
Simanta <simdon8848@gmail.com>
2020-02-08 11:42:54 +0545

    Test
    errors

--- animeweeb/login/tests.py
+++ animeweeb/login/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
-
+from . models import UserProfile
 # Create your tests here.
+
+#self Anime(self):
+#self.user = User.objects.create_user(username="Simanta",password="animeweeb",email="anime96@gmail.com")
