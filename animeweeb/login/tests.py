c0020ec81f31e670f52274c2757d5ca87a0e2efe
Simanta <simdon8848@gmail.com>
2020-02-10 14:15:18 +0545

    Testing
    Failed to create one

--- animeweeb/login/tests.py
+++ animeweeb/login/tests.py
@@ -2,5 +2,9 @@ from django.test import TestCase
 from . models import UserProfile
 # Create your tests here.
 
+      def test_dESC(self):
+        desc = Anime.objects.create(anime_title="chartpaper", upload_date="2002-06-02",
+                                      anime_description = "hi there whats up jhcv dcb d b ."
+                                      )
 #self Anime(self):
 #self.user = User.objects.create_user(username="Simanta",password="animeweeb",email="anime96@gmail.com")
