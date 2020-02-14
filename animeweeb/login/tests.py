2ff476f770938af3999157b44444cf10f544c04a
Simanta <simdon8848@gmail.com>
2020-02-14 18:57:10 +0545

    testing
    ran 2nd test case

--- animeweeb/login/tests.py
+++ animeweeb/login/tests.py
@@ -4,7 +4,7 @@ from .views import *
 from django.urls import reverse,resolve
 from django.contrib.auth.models import User
 
-class Test_URL(TestCase): #testing login
+class TestModel(TestCase): #testing login
     def test_user(self):
         user = User.objects.create_user(username="username", password="password1", email="email@gmail.com", first_name="first_name", last_name="last_name")
         user.save()
@@ -12,36 +12,16 @@ class Test_URL(TestCase): #testing login
         self.assertEqual(profile1.age,18)
         self.assertEqual(profile1.address,"handigau")
 
- #class Test_view(SimpleTestCase):   #testing views
-#   def test_register(self):
-#         c=Client()
-#         url=reverse("login:register") #reverse is used to show path 
-#         response=c.get(url)
-#         self.assertEquals(response.status_code,200)
-    
-class Test_animelist(TestCase):
-     def test_animelist(self):
-         url=reverse("login:anime")
-    
-         self.assertEquals(resolve(url).func,animelist)
-        
-     
-#     def test_login(self):   # testing urls
-#         c=Client()
-#         url=reverse("login:login")
-#         response=c.get(url)
-#         self.assertEquals(response.status_code,200)
+class TestURLS(TestCase):
+    def test_list_url_is_resolved(self):
+        url = reverse('login')
+        print(resolve(url).func,user_login)
 
-# class Test(TestCase): #Model testing     
-    # def test_dESC(self):
-    #     desc = Anime.objects.create(user="Animeislifenibb", address="nekoneko@gmail.com",
-    #                                   age = "50"
-    #                                   )
-    #     use = Anime.objects.get(user="Animeislifenibb")
-    #     self.assertEquals(use.user,"Animeislifenibb")
-    
-    # def test_title(self):
-    #     desc = Anime.objects.create(user="prashantiaunt", address="nekoneko@gmail.com",
-    #                                  age = "50"
-    #                                   )
-    #     self.assertTrue(desc.TestTitle())
+# class testViews(testcase):
+#     def test_views_weeb_GET(self):
+#         client = Client()
+
+#         response = clien.get(reverse('list'))
+
+#         self.assertEquals(response.status_code,200)
+#         self.asserTemplateUsed(response, 'login/list.html')
